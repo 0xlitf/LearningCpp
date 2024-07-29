eef2fa9fcb0b92e04b40b4f242ceae7cd8378ae6
0xlitf <0xlitf@gmail.com>
2024-07-29 16:57:13 +0800

    `

--- CppDatabaseInteract/UsingSqlcipher/main.cpp
+++ CppDatabaseInteract/UsingSqlcipher/main.cpp
@@ -1,7 +1,7 @@
 #include "sqlite3.h"
 #include <iostream>
 
-#pragma comment(lib, "sqlite3.lib")
+// #pragma comment(lib, "sqlite3.lib")
 
 int main() {
     // 打开或创建数据库文件
@@ -11,8 +11,8 @@ int main() {
     if (rc == SQLITE_OK) {
         std::cout << "open success" << std::endl;
     } else {
-            std::cerr << "open failed: " << sqlite3_errmsg(db) << std::endl;
-            return rc;
+        std::cerr << "open failed: " << sqlite3_errmsg(db) << std::endl;
+        return rc;
     }
 
     sqlite3_close(db);
